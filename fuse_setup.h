c3898177a7ce9a1945668d10ffa4dbd85ccc0ed7
hradec <me@hradec.com>
2018-03-29 20:18:49 -0700

    added a fuse_setup.h file to properly setup fuse. now using fuse 3.0!

---
+++ fuse_setup.h
@@ -0,0 +1,19 @@
+/*
+  HradecFS
+  Copyright (C) 2018 Roberto Hradec <me@hradec.com>
+
+  This program can be distributed under the terms of the GNU GPLv3.
+  See the file COPYING.
+
+  HradecFS is derived from "Big Brother File System by Joseph J. Pfeiffer".
+
+*/
+
+
+#ifndef FUSE_USE_VERSION
+
+#define FUSE_USE_VERSION 30
+
+#include <fuse3/fuse.h>
+
+#endif
